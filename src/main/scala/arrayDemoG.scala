aee80a05d7bef7b340b5e3a0729d194f6d3837c7
“Yor <yor.bonci@digitalturbine.com>
2024-04-03 14:40:44 +0200

    adding changes to day 14

--- src/main/scala/arrayDemoG.scala
+++ src/main/scala/arrayDemoG.scala
@@ -5,10 +5,13 @@ object arrayDemoG extends App{
   var marks1 = Array(5,6,8,9,2,4,6)
   var marks2 = Array(5,9,2,6,4,2,6)
 
-  var allMarks = concat(marks1, marks2) // concat is used to combine arrays to a single array
+//  var allMarks = concat(marks1, marks2) // concat is used to combine arrays to a single array
 
-  allMarks.foreach(println)
+  var allMarks = marks1 ++ marks2// concat is used to combine arrays to a single array
 
+  allMarks.foreach(n => print(s"$n "))
+
+  println("")
 
   var rollNo = range(1,101)
   rollNo.foreach(n => print(s"$n "))
